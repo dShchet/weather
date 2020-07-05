f676f0c73d5f28572cce68f7793bd72597626a2b
unknown <d.shchet@gmail.com>
2020-07-05 18:37:42 +0300

    Updates

---
+++ precache-manifest.8799a32be0451a7d90735a9faf79d22a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291b97732a85ae9cc5b283e42f58c030",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "42e98d4bc40eff93a9ae",
+    "url": "/weather/static/css/main.a89cf051.chunk.css"
+  },
+  {
+    "revision": "3a99dfa92d2123573a81",
+    "url": "/weather/static/js/2.8b12de56.chunk.js"
+  },
+  {
+    "revision": "42e98d4bc40eff93a9ae",
+    "url": "/weather/static/js/main.535c53e4.chunk.js"
+  },
+  {
+    "revision": "bcbbf36160edab700d41",
+    "url": "/weather/static/js/runtime-main.ee98893b.js"
+  }
+]);
